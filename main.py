47687fe362d62b8c0c90ca1610542757cd43f1c3
nizenkiy <66327241+nikitakv1-gif@users.noreply.github.com>
2025-05-31 13:37:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ application = app
 
 if __name__ == "__main__":
     # app.run(debug = True)
-    port = int(os.environ,get("PORT", 4000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host = "0.0.0.0", port = port)
